c0b790b4a1eebd9e0915a0854f639d6859897cb6
leo <fisherinaus@gmail.com>
2023-01-18 23:05:24 +1100

    d

--- WK3_Dockerisation/dockerfile/3.web-python-flask/run.sh
+++ WK3_Dockerisation/dockerfile/3.web-python-flask/run.sh
@@ -2,11 +2,11 @@
 set -e
 
 cd "$(dirname "$0")"
-
+docker network create -d bridge --subnet 172.25.0.0/16 mynet
 echo "Building image ..."
 docker build -t jr/web-citymatcher .
 
 echo
 echo "Running container ..."
 echo "Please open http://localhost?city=au to search cities whose name contains 'au'."
-docker run --name citymatcher -d --rm -p 80:80 jr/web-citymatcher
+docker run  --network mynet --network-alias citymatcher --name citymatcher -d --rm -p 80:80 jr/web-citymatcher
--- WK3_Dockerisation/dockerfile/4.console-dependency/app/main.sh
+++ WK3_Dockerisation/dockerfile/4.console-dependency/app/main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Press Ctrl+C any time to stop the application."
-#network 
-docker network connect mynet city 
+#network  docker network connect mynet citymatcher 
+
 echo
 
 for i in $(seq 2); do
